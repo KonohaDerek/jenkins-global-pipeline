2b6cf34d73e523933c6d3cb50c8e29d63cdd4431
derek.chen <derek.chen@iyes.dev>
2024-12-18 12:06:50 +0800

    feat: add failureWithAI

---
+++ vars/failureWithAI.groovy
@@ -0,0 +1,39 @@
+def call(Exception e) {
+  currentBuild.result = "FAILED"
+  env.buildColor = 'danger'
+   def slackUser = getSlackUser()
+   def message = """${env.namespace} ${env.project} ${env.GIT_BRANCH} Environment - BUILD ${currentBuild.result} after ${currentBuild.durationString.replace(' and counting', '')} (<${env.BUILD_URL}|Open>)
+  :warning: :warning: :warning: ${namespace} admin ${env.GIT_BRANCH} branch :warning: :warning: :warning:"""
+  def aiResponse = """ ``` ${getAssistantResponse(e.toString())} ``` """
+  // get the error trace
+  def err = """ ``` ${e.toString()}``` """
+
+  // send the message to slack
+  def isDevOps = aiResponse.contains("DevOps 處理")
+  def user = slackUser(isDevOps)
+  def attachments = [
+    [
+      text: message + user + aiResponse + err, 
+      fallback: 'Fallback',
+      color: "${env.buildColor}"
+    ]
+  ]
+
+ slackSend(channel: 'devops', color: "${env.buildColor}", attachments: attachments)
+}
+
+def getAssistantResponse(err) {
+      withCredentials([
+        string(credentialsId: 'AZURE_OPENAI_API_KEY', variable: 'AZURE_OPENAI_API_KEY'),
+        string(credentialsId: 'AZURE_OPENAI_ASSISTANT_ID', variable: 'AZURE_OPENAI_ASSISTANT_ID')]) {
+        def dockerCommand = """
+       docker run --rm -e NODE_ENV=production -e AZURE_OPENAI_ENDPOINT="https://howdigital-openai-us.openai.azure.com"  \
+ -e AZURE_OPENAI_API_KEY="${AZURE_OPENAI_API_KEY}"  \
+ -e AZURE_OPENAI_ASSISTANT_ID="${AZURE_OPENAI_ASSISTANT_ID}"  \
+ -e AZURE_OPENAI_MODEL="gpt-4o-mini" "asia-east1-docker.pkg.dev/howgroup-devops-2021/devops/jenkins-assistant-nest:1.0.2" -e ${err}
+       """
+        def msg = ""
+        msg = sh(script: dockerCommand, returnStdout: true)
+        return msg
+    }
+}
---
+++ vars/slackUser.groovy
@@ -0,0 +1,36 @@
+def call(Boolean isDevOps = false) {
+   def username = env.gitlabUserUsername || env.BUILD_USER_ID || 'derek'
+   if (isDevOps) {
+       username = 'derek.chen'
+   }
+   def userMap = [
+        // other
+        'phantom': '<@U01D2CKSPCK>',
+        'songo': '<@U01CVJQTQT1>',
+        // DevOps
+        'derek': '<@U06MHPF6VCN>', // jenkins
+        'derek.chen': '<@U06MHPF6VCN>' // gitlab
+        // RD
+        'mandy': '<@U01DESVR19P>', // jenkins
+        'naremloa': '<@U02B2N35Y2J>', // jenkins
+        'jeeying': '<@U01QG8K4K1Q>',// jenkins
+        'jeeying.chen': '<@U01QG8K4K1Q>', // gitlab
+        'gene.ke': '<@U04GGLDFPQQ>', // gitlab
+        'ray':'<@U02NFKVMVAP>', // jenkins
+        'ray.tsai': '<@U02NFKVMVAP>', // gitlab
+        'bill.cheng': '<@U04US7Y3K44>' // gitlab
+        'aura.huang': '<@U062FUSQKUN>' // gitlab
+        'ellen': '<@U03U59J1SRF>' // gitlab
+    ]
+
+    // 檢查 username 是否存在於 Map 中
+    if (myMap.containsKey(username)) {
+        def value = myMap[username]
+        println "Value for ${username}: ${value}"
+        return value
+    } else {
+         println "Username ${username} not found in the map."
+        // 如果沒有找到，則回傳預設值
+        return myMap[ 'derek']
+    }
+}
