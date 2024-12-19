0571aa451a72cd067ea0cd328351d02c1aa372c8
derek.chen <derek.chen@iyes.dev>
2024-12-19 18:20:59 +0800

    feat: update jenkins-assistant-nest version

--- vars/failureWithAI.groovy
+++ vars/failureWithAI.groovy
@@ -34,7 +34,8 @@ def getAssistantResponse(err) {
        docker run --rm -e NODE_ENV=production -e AZURE_OPENAI_ENDPOINT="https://howdigital-openai-us.openai.azure.com"  \
  -e AZURE_OPENAI_API_KEY="${AZURE_OPENAI_API_KEY}"  \
  -e AZURE_OPENAI_ASSISTANT_ID="${AZURE_OPENAI_ASSISTANT_ID}"  \
- -e AZURE_OPENAI_MODEL="gpt-4o-mini" "asia-east1-docker.pkg.dev/howgroup-devops-2021/devops/jenkins-assistant-nest:1.0.2" -e ${err}
+ -e AZURE_OPENAI_MODEL="gpt-4o-mini" \
+ -e AZURE_OPENAI_API_VERSION="2024-07-01-preview" "asia-east1-docker.pkg.dev/howgroup-devops-2021/devops/jenkins-assistant-nest:1.0.3" -e ${err}
        """
         def msg = ""
         msg = sh(script: dockerCommand, returnStdout: true)
